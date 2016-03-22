bb9333b1d3c850cb58b900f389c370d89bf3dff6
John Yuan <2413490599@qq.com>
2016-03-22 20:13:31 +0800

    Daily

--- Shell/expect.sh
+++ Shell/expect.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/expect
 set timeout 30
-spawn su -
+spawn  inotifywait -rqm --timefmt '%Y%m%d%H%M%S' --format '%T,%w%f,%e' -e modify,create,delete,attrib,moved_to,moved_from "./be/" | while read FL
+do
+    echo FL
+    rsync -avz --exclude "*/CMakeFiles/" --exclude "*/gen_src/" --exclude "*/build/" /home/john/Work/trunk/be/ yuanwenxing@192.168.8.189:/home/yuanwenxing/XCloud_V4/trunk/be/
+done
 expect ":"
 send "ubuntu\r"
 interact
